d7e0f16eb05d4fa7cca9394ca763356217fd9ecb
malidu04 <malidupahasara04@gmail.com>
2025-12-20 00:41:38 +0530

    config/database.js file creation

---
+++ backend/src/config/database.js
@@ -0,0 +1,19 @@
+export default {
+    dialect: process.env.DB_DIALECT || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'ml_platform',
+    logging: process.env.NODE_ENV === 'development',
+    define: {
+        timestamps: true,
+        underscored: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
