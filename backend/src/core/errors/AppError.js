c69fd7a6fc66473e438233a8c91956d6424468af
malidu04 <malidupahasara04@gmail.com>
2025-12-20 09:01:52 +0530

    core/errors/AppError.js file creation

---
+++ backend/src/core/errors/AppError.js
@@ -0,0 +1,10 @@
+export class AppError extends Error {
+    constructor (message, statusCode, isOperational = true) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = isOperational;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
