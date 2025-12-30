f84bf59130bf8ccc4e24bbb354048b0218d74a42
malidu04 <malidupahasara04@gmail.com>
2025-12-30 23:08:47 +0530

    controllers/authController.js file creation

---
+++ backend/src/controllers/modelController.js
@@ -0,0 +1,8 @@
+import { AppError } from '../core/errors/AppError.js';
+import logger from '../core/utils/logger.js';
+import modelService from '../services/modelService.js';
+
+
+class ModelController {
+    async 
+}
